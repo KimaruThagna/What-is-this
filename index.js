c0664e6e44e119e8c39633f9b7bc5b07805afadb
Kimaru thagana <macbuntu@localhost.localdomain>
2019-08-28 14:51:41 +0300

    image classifier ueing tensorflow.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const classifier = knnClassifier.create();
 const webcamElement = document.getElementById('webcam');
 let net;
-
 async function app() {
   console.log('Loading mobilenet..');
 
@@ -44,4 +43,24 @@ async function app() {
     await tf.nextFrame();
   }
 }
+
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
+
 app();
